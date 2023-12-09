b902061a91b84a71753613fe216bea2bd5e008d4
MayelNdiaye <mayelndiayeinfo@gmail.com>
2023-12-10 00:00:39 +0100

    fuzzy finder

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 require("xrobotx")
 print("hello")
-vim.cmd('colorscheme moonlight')
+vim.cmd('colorscheme embark')
--- lua/xrobotx/packer.lua
+++ lua/xrobotx/packer.lua
@@ -43,4 +43,9 @@ use {
     "williamboman/mason.nvim"
 }
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
 end)
